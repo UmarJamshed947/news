db59ed0aece9c27b39f782ee22d2e7cd127a4359
Umar Jamshed <128473349+UmarJamshed947@users.noreply.github.com>
2023-08-02 14:40:30 +0500

    app icon changed
    app icon changed

--- lib/helper/category_data.dart
+++ lib/helper/category_data.dart
@@ -6,13 +6,13 @@ List<CategoryModel> getCategories(){
   CategoryModel categoryModel = new CategoryModel();
 
   //1
-  categoryModel.categoryName = "Bussiness";
+  categoryModel.categoryName = "Business";
   categoryModel.imageUrl = "https://images.unsplash.com/photo-1612550761236-e813928f7271?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80";
   category.add(categoryModel);
   //2
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Entertainment";
-  categoryModel.imageUrl = "https://images.unsplash.com/photo-1496337589254-7e19d01cec44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1598387181032-a3103a2db5b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1476&q=80";
   category.add(categoryModel);
   //3
   categoryModel = new CategoryModel();
@@ -22,12 +22,12 @@ List<CategoryModel> getCategories(){
   //4
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Health";
-  categoryModel.imageUrl = "https://images.unsplash.com/photo-1505751172876-fa1923c5c528?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80";
   category.add(categoryModel);
   //5
   categoryModel = new CategoryModel();
   categoryModel.categoryName = "Technology";
-  categoryModel.imageUrl = "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1563770660941-20978e870e26?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80";
   category.add(categoryModel);
   return category;
 }
